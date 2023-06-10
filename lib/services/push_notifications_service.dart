0821461a1a624fc5a3055697290134139b712406
faldado82 <faldado82@gmail.com>
2023-06-10 14:59:11 -0300

    Stream controller, firebase Notifications

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,29 +1,41 @@
 // SHA-1: 91:2A:34:E2:2B:7A:EA:86:9D:3E:7F:51:CA:2D:4C:99:80:F4:EF:66
 
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static final StreamController<String> _messageStreamController =
+      StreamController.broadcast();
+
+  static Stream<String> get messagesStream => _messageStreamController.stream;
 
   static Future _onBackgroundHandler(RemoteMessage message) async {
-    print('onBackground Handler ${message.messageId}');
+    // print('onBackground Handler ${message.messageId}');
+    print(message.data);
+    _messageStreamController.add(message.notification?.title ?? 'No title');
   }
 
   static Future _onMessageHandler(RemoteMessage message) async {
-    print('onMessage Handler ${message.messageId}');
+    // print('onMessage Handler ${message.messageId}');
+    print(message.data);
+    _messageStreamController.add(message.notification?.title ?? 'No title');
   }
 
   static Future _onMessageOpenAppHandler(RemoteMessage message) async {
-    print('onMessageOpenApp Handler ${message.messageId}');
+    // print('onMessageOpenApp Handler ${message.messageId}');
+    print(message.data);
+    _messageStreamController.add(message.notification?.title ?? 'No title');
   }
 
   static Future initializeApp() async {
     // Push Notifications
     await Firebase.initializeApp();
     token = await FirebaseMessaging.instance.getToken();
-    print(token);
+    print('TOKEN: $token');
 
     // Handlers
     FirebaseMessaging.onBackgroundMessage(_onBackgroundHandler);
@@ -31,5 +43,9 @@ class PushNotificationService {
     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenAppHandler);
 
     // Local Notifications
+  }
+
+  static closeStreams() {
+    _messageStreamController.close();
   }
 }
